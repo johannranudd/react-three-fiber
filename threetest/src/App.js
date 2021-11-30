f46f6e139032ebed18c5c00d4f3e00764eb9fa5f
johannranudd <33978097+johannranudd@users.noreply.github.com>
2021-11-30 15:29:46 +0100

    simple sphere that rotates

--- threetest/src/App.js
+++ threetest/src/App.js
@@ -1,27 +1,28 @@
 import React from 'react';
-import { Grid, Box } from '@chakra-ui/layout';
+import { Box } from '@chakra-ui/layout';
 import { Canvas } from '@react-three/fiber';
 import { OrbitControls, Stars } from '@react-three/drei';
-// import Sphere from './components/Sphere';
-import Cube from './components/Cube';
-import Plane from './components/Plane';
-import { Physics } from '@react-three/cannon';
+import Sphere from './components/Sphere';
+// import Cube from './components/Cube';
+// import Plane from './components/Plane';
+// import { Physics } from '@react-three/cannon';
 // import SkyBox from './components/SkyBox';
 
 function App() {
   return (
     <>
       <Box h='100vh' w='100%'>
-        <Canvas>
-          {/* lights */}
-          <pointLight intensity={0.3} position={[10, 0, 0]} />
-          <pointLight intensity={0.3} position={[0, 0, 10]} />
-          <pointLight intensity={0.3} position={[0, 0, 0]} />
-          <directionalLight intensity={1} position={[100, 100, 100]} />
-          {/* objects */}
+        <Canvas camera={{ position: [0, 0, 10] }}>
+          {/* controls */}
           <OrbitControls />
-          <Cube />
-          <Plane />
+          {/* lights */}
+          {/* <pointLight intensity={0.3} position={[10, 0, 0]} /> */}
+          {/* <pointLight intensity={0.3} position={[0, 0, 10]} /> */}
+          {/* <pointLight intensity={0.3} position={[0, 0, 0]} /> */}
+          <directionalLight intensity={1} position={[100, 0, 180]} />
+          {/* objects */}
+          <Stars />
+          <Sphere />
         </Canvas>
       </Box>
     </>
--- threetest/src/components/Sphere.js
+++ threetest/src/components/Sphere.js
@@ -1,35 +1,15 @@
-import React from 'react';
-import { useFrame, useThree } from '@react-three/fiber';
-import {
-  WebGLCubeRenderTarget,
-  RGBFormat,
-  LinearMipMapLinearFilter,
-  CubeCamera,
-} from 'three';
+import React, { useRef } from 'react';
+import { useFrame } from '@react-three/fiber';
 
 const Sphere = () => {
-  const { gl, scene } = useThree();
-  const cubeRenderTarget = new WebGLCubeRenderTarget(512, {
-    format: RGBFormat,
-    generateMipmaps: true,
-    minFilter: LinearMipMapLinearFilter,
+  const sphereRef = useRef(null);
+  useFrame(() => {
+    sphereRef.current.rotation.y += 0.003;
   });
-  const cubeCamera = new CubeCamera(1, 100, cubeRenderTarget);
-  cubeCamera.position.set(0, 0, 0);
-  scene.add(cubeCamera);
-
-  useFrame(() => cubeCamera.update(gl, scene));
-
   return (
-    <mesh visible position={[0, 0, 0]} rotation={[0, 0, 0]} castShadow>
-      <sphereGeometry attach='geometry' args={[2, 100, 100]} />
-      <meshLambertMaterial
-        attach='material'
-        envMap={cubeCamera.renderTarget.texture}
-        color='white'
-        roughtness={0.1}
-        metalness={1}
-      />
+    <mesh ref={sphereRef} position={[0, 0, 0]} castShadow>
+      <sphereGeometry attach='geometry' args={[2, 1, 1]} />
+      <meshLambertMaterial attach='material' color='white' />
     </mesh>
   );
 };
